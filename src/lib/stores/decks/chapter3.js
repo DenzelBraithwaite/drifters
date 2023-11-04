287750c782698e540fe318fbab80e1365f148d69
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-11-04 08:22:40 -0400

    adding dialogue for chapter 3, finished goblins

--- src/lib/stores/decks/chapter3.js
+++ src/lib/stores/decks/chapter3.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
 const chapter3 = writable([
-        // Wizard
+        // Wizard (twins irl with dark wizard)
         {
             id: 'chapter3-1a',
             imgUrl: '/decks/chapter3/wizard.png',
@@ -22,16 +22,176 @@ const chapter3 = writable([
             },
             location: 'Arcana Kingdom'
         },
+        {
+            id: 'chapter3-1b',
+            imgUrl: '/decks/chapter3/wizard.png',
+            title:'Geldahn',
+            text: "Sometimes, I wonder if I should have become something other than a wizard. Maybe a farmer?",
+            faction: 'Wizard',
+            textLeft: "I think being a wizard is a lot more badass than being some farmer.",
+            textRight: "I respect those who tend to animals, perhaps there's a way for you to do both.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1c',
+            imgUrl: '/decks/chapter3/wizard.png',
+            title:'Geldahn',
+            text: "I feel connected to someone in Arcana kingdom, I feel them searching for me. Should I avoid them or let them find me?",
+            faction: 'Wizard',
+            textLeft: "Go with your gut feeling, if you think they mean you harm then fight back. If you're afraid then run. If you're unsure then wait.",
+            textRight: "Perhaps you can find them before they can find you.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1d',
+            imgUrl: '/decks/chapter3/wizard.png',
+            title:'Geldahn',
+            text: "I call myself a wizard, but I don't know if I've ever cast a spell before. Am I truly a wizard?",
+            faction: 'Wizard',
+            textLeft: "Probably not, most people aren't who they seem in this world. We're all just struggling to remember.",
+            textRight: "As long as nobody else knows.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1e',
+            imgUrl: '/decks/chapter3/wizard.png',
+            title:'Geldahn',
+            text: "I feel him... He's been searching for me since we arrived. We arrived here together didn't we?",
+            faction: 'Wizard',
+            textLeft: "I don't know.",
+            textRight: "Go to him.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
 
-        // Dark wizard
+        // Dark wizard (twins irl with wizard)
         {
             id: 'chapter3-1b',
             imgUrl: '/decks/chapter3/dark_wizard.png',
-            title:'Geldahn?',
+            title:'Gahndel',
             text: "You might have met an older wizard who resembles me, I'd appreciate it if you kept our little interaction a secret.",
             faction: 'Wizard',
             textLeft: "Your secret is safe with me.",
             textRight: "You seem like you're up to no good.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1b',
+            imgUrl: '/decks/chapter3/dark_wizard.png',
+            title:'Gahndel',
+            text: "The wizard who resembles me, I don't want to frighten him. But I feel as if I must watch over him.",
+            faction: 'Wizard',
+            textLeft: "Perhaps you should simply go talk to him.",
+            textRight: "Yeah stalking from afar can't possibly end poorly.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1c',
+            imgUrl: '/decks/chapter3/dark_wizard.png',
+            title:'Gahndel',
+            text: "I've been called Gahndel, but something feels wrong with that name. The other wizard is Geldahn isn't he? Why do I know that?",
+            faction: 'Wizard',
+            textLeft: "Perhaps you two are one soul divided.",
+            textRight: "Maybe he's family?",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1d',
+            imgUrl: '/decks/chapter3/dark_wizard.png',
+            title:'Gahndel',
+            text: "There are times I feel as if I've always been a wizard, but I can't seem to case a single spell. Perhaps you could teach me?",
+            faction: 'Wizard',
+            textLeft: "Time and reality are very strange here, but I'm pretty sure I can't teach you magic.",
+            textRight: "Grip your staff and focus inward on your anger festering in your stomach, then release it all at once to create a fireball.",
+            actionLeft: {
+                health: 0,
+                aura: 0,
+                memory: 0,
+            },
+            actionRight: {
+                health: 0,
+                memory: 0,
+                aura: 0,
+            },
+            location: 'Arcana Kingdom'
+        },
+        {
+            id: 'chapter3-1e',
+            imgUrl: '/decks/chapter3/dark_wizard.png',
+            title:'Gahndel',
+            text: "I'm Gahndel the wizard, I specialize in the fireball. Truth is, it's the only spell I've ever known. To cast fire, focus inward on your anger festering in your stomach, then release it all at once to create a fireball!",
+            faction: 'Wizard',
+            textLeft: "This is weird, you don't remember me? Didn't I teach you that, or are you teaching me now?",
+            textRight: "That's impressive, I'll have to give it a try one day.",
             actionLeft: {
                 health: 0,
                 aura: 0,
--- src/lib/stores/decks/goblins.js
+++ src/lib/stores/decks/goblins.js
@@ -1,15 +1,15 @@
 import { writable } from 'svelte/store';
 
 const goblins = writable([
-    // Zig
+    // Zig, a kid who doesn't know why he's in this form and wants his mom and dad
     {
         id: 'chapter3-g-1a',
-        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
         title:'Zig',
-        text: "Looky what we have here, a human.",
+        text: "Ten imi's 'Zig', niji tam meet kahl. Kahl aren't as scarrit as sep othar monsters ket.",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "Zig is it? Are you calling me a monster?",
+        textRight: "The other monsters... scare you?",
         actionLeft: {
             health: 0,
             aura: 0,
@@ -23,12 +23,12 @@ const goblins = writable([
     },
     {
         id: 'chapter3-g-1b',
-        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
         title:'Sting',
-        text: "Looky what we have here, a human.",
+        text: "Kahl speak tam ae goblin without knowtin ko tam speak gobbledegook?",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "Gobblede- what now?",
+        textRight: "I don't know Gobbledegook but I think I can still understand you.",
         actionLeft: {
             health: 0,
             aura: 0,
@@ -40,16 +40,150 @@ const goblins = writable([
             aura: 0,
         }
     },
+    {
+        id: 'chapter3-g-1c',
+        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
+        title:'Sting',
+        text: "Pievre hilp, ti don't awet mea ti'm talktin kat mei. Ti don't awet sto's gotin nef!",
+        faction: 'Goblin',
+        textLeft: "I'm sorry I just... I don't understand.",
+        textRight: "Are you... asking for help of some kind?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-1d',
+        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
+        title:'Sting',
+        text: "Wheke es ten pan el pon? mea vie't ti reise ou?",
+        faction: 'Goblin',
+        textLeft: "What?",
+        textRight: "Pan el Pon... Your mom and dad? Wait slow down.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-1e',
+        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
+        title:'Sting',
+        text: "Kahl ade mei tam si, kahl ade.",
+        faction: 'Goblin',
+        textLeft: "Oogey, oh boogey, tinka bop boom bang? Did that mean anything?",
+        textRight: "Why does it feel like you're blaming me for something?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    
 
-    // Craz
+    // Craz, an average goblin who hates humans
     {
         id: 'chapter3-g-2a',
-        imgUrl: '/decks/chapter3/goblins/goblin-child.png',
+        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
         title:'Craz',
-        text: "Looky what we have here, a human.",
+        text: "Kahl look sef faamilir, mea es spet?",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "Am I your familiar pet? Is that what you think I am?",
+        textRight: "I look... like family? I look... familiar maybe? ",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-2b',
+        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        title:'Craz',
+        text: "Kahl think ti des tam be kat kahl? ti'm stuck kat mei itst.",
+        faction: 'Goblin',
+        textLeft: "Does anyone in your clan speak any English?",
+        textRight: "I can't shake the feeling you goblins are trying to warn me of something.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-2c',
+        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        title:'Craz',
+        text: "Ak kahl don't absc mei place, ti'll det kahl wheke kahl stand!",
+        faction: 'Goblin',
+        textLeft: "Calm down, I don't mean any harm.",
+        textRight: "Why do so many creatures hate humans, or is it me they hate?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-2d',
+        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        title:'Craz',
+        text: "Kahl marit ek remembar everytee, but spet no mean not'et forgettin.",
+        faction: 'Goblin',
+        textLeft: "I am forgetting everything?",
+        textRight: "You remember everything?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-2e',
+        imgUrl: '/decks/chapter3/goblins/goblin-blade.png',
+        title:'Craz',
+        text: "Kahl ha ae Keelar, ullwee het be el ullwee vol be.",
+        faction: 'Goblin',
+        textLeft: "Please, help me understand you.",
+        textRight: "Enough with the gibberish, I don't have time for this.",
         actionLeft: {
             health: 0,
             aura: 0,
@@ -62,15 +196,91 @@ const goblins = writable([
         }
     },
 
-    // Ruck
+    // Ruck, an old goblin who has many memories and offers insight about the world.
     {
         id: 'chapter3-g-3a',
         imgUrl: '/decks/chapter3/goblins/goblin-old.png',
         title:'Ruck',
-        text: "Looky what we have here, a human.",
+        text: "Maal, ten imi es Ruck. Kahl aren't from around ket are kahl, yet ti'et seen kahl somewheke befoke.",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "Hey, I can actually understand what you're saying! You recognize me?",
+        textRight: "Is this GobbldeEnglish? You speak differently from the other goblins, nice to meet you Maal.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-3b',
+        imgUrl: '/decks/chapter3/goblins/goblin-old.png',
+        title:'Ruck',
+        text: "Ti het lived ae long live wit lot memories, sef naturallrit ti able tam tell mei es no from ten world.",
+        faction: 'Goblin',
+        textLeft: "It seems there are some of us who have memories before this place, you seem to remember more than most.",
+        textRight: "So you think the older you are, the more memories you retain?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-3c',
+        imgUrl: '/decks/chapter3/goblins/goblin-old.png',
+        title:'Ruck',
+        text: "Ti knew ten time neb come, but et seems kahl knew moar than ti do. But ti bet kahl no think kahl be here wit Ruck.",
+        faction: 'Goblin',
+        textLeft: "Your time? How could I know more about your time than you do, I don't understand.",
+        textRight: "You are Ruck, I am stuck with you? No, I am here with you? Why should that surprise me?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-3d',
+        imgUrl: '/decks/chapter3/goblins/goblin-old.png',
+        title:'Ruck',
+        text: "Ti marit be stuck ehn mei fate, but kahl vol suffar far moar dan we will.",
+        faction: 'Goblin',
+        textLeft: "I'm going to suffer more than the others? Why me, I don't understand.",
+        textRight: "Why am I so important? What is Jude not telling me.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-3e',
+        imgUrl: '/decks/chapter3/goblins/goblin-old.png',
+        title:'Ruck',
+        text: "Ovar el ovar el ovar again, we relivtin sep same days, saytin sep same sreth. Ti probablrit alreadrit said mei tam kahl but ti no remembar. Dis world no onlrit yours, et's Jude world oso. Jude simplrit remembar moar dan kahl remembar.",
+        faction: 'Goblin',
+        textLeft: "Repeating the same days over and over? But why would Jude want that, it doesn't make sense.",
+        textRight: "This is not only my world, but Jude's world... Then why isn't he telling me anything?",
         actionLeft: {
             health: 0,
             aura: 0,
@@ -83,15 +293,15 @@ const goblins = writable([
         }
     },
 
-    // Gerg
+    // Gerg, an average goblin who hates humans.
     {
         id: 'chapter3-g-4a',
         imgUrl: '/decks/chapter3/goblins/goblin-ponytail.png',
         title:'Gerg',
-        text: "Looky what we have here, a human.",
+        text: "Arg! Trav aawarit filthrit human.",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "*Walk away*",
+        textRight: "You're no different than the elves.",
         actionLeft: {
             health: 0,
             aura: 0,
@@ -103,16 +313,169 @@ const goblins = writable([
             aura: 0,
         }
     },
+    {
+        id: 'chapter3-g-4b',
+        imgUrl: '/decks/chapter3/goblins/goblin-ponytail.png',
+        title:'Gerg',
+        text: "Get farr from ket, mei es our land. Goblin, no human!",
+        faction: 'Goblin',
+        textLeft: "You don't even know me, or how to speak properly.",
+        textRight: "Maybe I don't want to leave, maybe no goblin!",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-4c',
+        imgUrl: '/decks/chapter3/goblins/goblin-ponytail.png',
+        title:'Gerg',
+        text: "Sep capp za ae human vie be use as ae trophrit affix tam sep tail za ahn's weapon.",
+        faction: 'Goblin',
+        textLeft: "Whatever that was, it sounded threatening.",
+        textRight: "Human trophies? Oh hell no!",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-4d',
+        imgUrl: '/decks/chapter3/goblins/goblin-ponytail.png',
+        title:'Gerg',
+        text: "Goblins ha no understand, encam not des tam fal es fier villages el det humans. Et's nie no same dan sto humans fal.",
+        faction: 'Goblin',
+        textLeft: "By now I'd have hoped to understand some of your language, but I can't seem to grasp what you're saying.",
+        textRight: "Fier, villages, humans, humans fal? Sounds like you want to pick a fight.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-4e',
+        imgUrl: '/decks/chapter3/goblins/goblin-ponytail.png',
+        title:'Gerg',
+        text: "Kahl ha sep cause ten encam za mei, ek humans, kahl.",
+        faction: 'Goblin',
+        textLeft: "...",
+        textRight: "Kahl is 'you', but I can't figure out the rest. What is 'ek' I wonder?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    
 
     // Oorh
     {
         id: 'chapter3-g-5a',
         imgUrl: '/decks/chapter3/goblins/goblin-woman.png',
         title:'Oorh',
-        text: "Looky what we have here, a human.",
+        text: "Hi there, I'm Oorh. No need to worry, I'm not like the other Goblins.",
         faction: 'Goblin',
-        textLeft: "*Run away*",
-        textRight: "I hope all goblins aren't this ugly.",
+        textLeft: "You speak perfect English!",
+        textRight: "Oh thank god, it seemed like they all wanted to kill me.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-5b',
+        imgUrl: '/decks/chapter3/goblins/goblin-woman.png',
+        title:'Oorh',
+        text: "If you notice the goblins feel threatened by you, it's only natural. Many of them are confused and afraid or angry.",
+        faction: 'Goblin',
+        textLeft: "I guess that makes sense, but it felt personal in some cases.",
+        textRight: "There was one intriguing old man, Ruck I believe. He seemed to understand more than the rest of us.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-5c',
+        imgUrl: '/decks/chapter3/goblins/goblin-woman.png',
+        title:'Oorh',
+        text: "Ruck and I may seem different, but I don't think that's true. I think all of us are the same, Ruck and I just happen to remember more.",
+        faction: 'Goblin',
+        textLeft: "Wouldn't that make you different then?",
+        textRight: "I wish I knew why some have more memories than others. Is it time, age, personality?",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-5d',
+        imgUrl: '/decks/chapter3/goblins/goblin-woman.png',
+        title:'Oorh',
+        text: "Memory is a tricky thing, do we remember more or do we have more false memories?",
+        faction: 'Goblin',
+        textLeft: "You must remember more.",
+        textRight: "Perhaps you're right and you actually remember less.",
+        actionLeft: {
+            health: 0,
+            aura: 0,
+            memory: 0,
+        },
+        actionRight: {
+            health: 0,
+            memory: 0,
+            aura: 0,
+        }
+    },
+    {
+        id: 'chapter3-g-5e',
+        imgUrl: '/decks/chapter3/goblins/goblin-woman.png',
+        title:'Oorh',
+        text: "Whatever is going on, you and Jude seem to be at the center of it. It seems we're stuck here until you regain your memories.",
+        faction: 'Goblin',
+        textLeft: "You know about Jude...",
+        textRight: "My memories are the key to releasing you?",
         actionLeft: {
             health: 0,
             aura: 0,
